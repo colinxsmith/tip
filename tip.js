169dfc2e24fed192058e80a74323ced144199b65
Colin <colinxsmith@gmail.com>
2025-01-02 18:03:00 +0000

    set border radius with this method

--- tip.js
+++ tip.js
@@ -1,3 +1,4 @@
+
 const tooltip = document.querySelector(".tooltip");
 
 // Base Slider
@@ -23,6 +24,17 @@ heightInput.addEventListener("input", (event) => {
   heightLabelValue.innerHTML = value;
   tooltip.style.setProperty("--triangle-height", value);
 });
+// Top Slider
+
+const topInput = document.querySelector("#top");
+const topLabelValue = document.querySelector("label[for='top'] .value");
+
+topInput.addEventListener("input", (event) => {
+  const value = `${event.target.value}%`;
+
+  topLabelValue.innerHTML = value;
+  tooltip.style.setProperty("--triangle-top", value);
+});
 
 // Left Slider
 
@@ -36,58 +48,13 @@ leftInput.addEventListener("input", (event) => {
   tooltip.style.setProperty("--triangle-left", value);
 });
 
-
-//T Left Slider
-
-const tleftInput = document.querySelector("#tleft");
-const tleftLabelValue = document.querySelector("label[for='tleft'] .value");
-
-tleftInput.addEventListener("input", (event) => {
-  const value = `${event.target.value}%`;
-
-  tleftLabelValue.innerHTML = value;
-  tooltip.style.setProperty("--tip-left", value);
-});
-//T Top Slider
-
-const ttopInput = document.querySelector("#ttop");
-const ttopLabelValue = document.querySelector("label[for='ttop'] .value");
-
-ttopInput.addEventListener("input", (event) => {
-  const value = `${event.target.value}px`;
-
-  ttopLabelValue.innerHTML = value;
-  tooltip.style.setProperty("--tip-top", value);
-});
-//T Position Radio
-
-const tabsoluteInput = document.querySelector("#tabsolute");
-const tabsoluteValue = document.querySelector("label[for='tabsolute'] .value");
-const trelativeInput = document.querySelector("#trelative");
-const trelativeValue = document.querySelector("label[for='trelative'] .value");
-
-tabsoluteInput.addEventListener("input", (event) => {
-  const value = `${event.target.value}`;
-console.log(value);
-  tabsoluteValue.innerHTML = value;
-  tooltip.style.setProperty("--tip-position", value);
-});
-trelativeInput.addEventListener("input", (event) => {
-  const value = `${event.target.value}`;
-
-  console.log(value);
-  trelativeValue.innerHTML = value;
-  tooltip.style.setProperty("--tip-position", value);
-});
 // Reset
 
 const resetButton = document.querySelector(".reset");
-const baseDefault = "30px";
-const heightDefault = "15px";
+const baseDefault = "14px";
+const heightDefault = "10px";
+const topDefault = "100%";
 const leftDefault = "50%";
-const tleftDefault = "0%";
-const ttopDefault = "0px";
-const tpositionDefault="absolute";
 
 resetButton.addEventListener("click", (event) => {
   baseLabelValue.innerHTML = baseDefault;
@@ -96,15 +63,9 @@ resetButton.addEventListener("click", (event) => {
   heightLabelValue.innerHTML = heightDefault;
   tooltip.style.setProperty("--triangle-height", heightDefault);
 
+  topLabelValue.innerHTML = topDefault;
+  tooltip.style.setProperty("--triangle-top", topDefault);
+
   leftLabelValue.innerHTML = leftDefault;
   tooltip.style.setProperty("--triangle-left", leftDefault);
- 
-  tleftLabelValue.innerHTML = tleftDefault;
-  tooltip.style.setProperty("--tip-left", tleftDefault);
-
-  ttopLabelValue.innerHTML = ttopDefault;
-  tooltip.style.setProperty("--tip-top", ttopDefault);
-
-  tabsoluteValue.innerHTML = tpositionDefault;
-  tooltip.style.setProperty("--tip-position", tpositionDefault);
 });
